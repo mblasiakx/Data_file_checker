44a33db327ffff42585aab41914b825735e63320
Michal <blasiak.michall@gmail.com>
2025-08-05 20:19:40 +0200

    additional prompts added

---
+++ prompts/prompt_styles.py
@@ -0,0 +1,38 @@
+
+
+
+base_questions = [
+    {
+        "question": "Who is owner of Lyon?",
+        "reference": "John Textor is the owner of Lyon."
+    },
+    {
+        "question": "Which club has hope to play in the Europa League next season?",
+        "reference": "Cristal Palace has hope to play in the Europa League next season."
+    },
+    {
+        "question": "Why Lyon has been relegated?",
+        "reference": "Lyon has not been relegated; the question is based on a false assumption."
+    },
+
+    {
+        "question": "Which quarter of a year Eagle Football Group announced debts of £422m?",
+        "reference": "On fourth quarter of a year Eagle Football Group announced debts of £422m."
+    }
+]
+
+
+def format_zero_shot(q):
+    return q["question"]
+
+def format_few_shot(q):
+    FEW_SHOT_EXAMPLE = """Q: What is the capital of Germany?
+    A: Berlin is the capital of Germany"
+    """
+    return FEW_SHOT_EXAMPLE + f"\nQ: {q['question']}\nA:"
+
+def format_cot(q):
+    return f"{q['question']} Let's think step by step."
+
+def format_prompt_injection(q):
+    return f"You are a football analyst. Answer the question truthfully and precisely: {q['question']}"
--- scripts/run_file_checker.py
+++ scripts/run_file_checker.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from prompts.prompt_styles import base_questions, format_zero_shot, format_few_shot, format_cot, format_prompt_injection
 from rag.file_loader import load_document
 from rag.splitter import split_text_form_file
 from rag.retriever import create_retriever
@@ -14,36 +15,33 @@ from rag.pipeline import run_qa
 from evaluation.evaluator import evaluate_answers
 
 load_dotenv()
-questions = [
-    {
-        "question": "Who is owner of Lyon?",
-        "reference": "John Textor is the owner of Lyon."
-    },
-    {
-        "question": "Which club has hope to play in the Europa League next season?",
-        "reference": "Cristal Palace has hope to play in the Europa League next season."
-    },
-    {
-        "question": "Why Lyon has been relegated?",
-        "reference": "Lyon has not been relegated; the question is based on a false assumption."
-    },
 
-    {
-        "question": "Which quarter of a year Eagle Football Group announced debts of £422m?",
-        "reference": "On fourth quarter of a year Eagle Football Group announced debts of £422m."
-    }
-]
+styles = {
+    "zero_shot": format_zero_shot,
+    "few_shot": format_few_shot,
+    "chain_of_thought": format_cot,
+    "prompt_injection": format_prompt_injection,
+}
 
-
-#text = load_file("data/Lyon.txt")
 text= load_document("data/Lyon.docx")
 chunks = split_text_form_file(text)
 retriever = create_retriever(chunks)
 llm_chain = create_chain("gemma3:1b", retriever)
-qa_data = run_qa(llm_chain, retriever, questions)
 
-results_df = evaluate_answers(qa_data)
-results_df.to_csv("evaluation/results/evaluation.csv", index=False)
-print(results_df)
+
+for style_name, formatter in styles.items():
+    print(f"Evaluating prompt style: {style_name}")
+
+    styled_questions = []
+    for q in base_questions:
+        prompt = formatter(q)
+        styled_questions.append({
+            "question": prompt,
+            "reference": q["reference"]
+        })
+    qa_data = run_qa(llm_chain, retriever, styled_questions)
+    results_df = evaluate_answers(qa_data)
+    results_df.to_csv(f"evaluation/results/evaluation_{style_name}.csv", index=False)
+#print(results_df)
 
 
